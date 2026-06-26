6a92f3fc5f0e025202879bc2aa28eb483f7f7cab
Jhonatan Gonzaga <jhonatangonzagagaldino@gmail.com>
2026-06-26 08:59:23 -0400

    feat: ajuste de telas

--- TP4-mvp/front-end/src/pages/index.ts
+++ TP4-mvp/front-end/src/pages/index.ts
@@ -4,4 +4,4 @@ export { PhoneVerificationScreen } from "./login-telefone";
 export { GoogleSignInScreen } from "./login-gmail";
 export { ProfileChoiceScreen } from "./escolher-perfil";
 export { AccountProfileScreen } from "./perfil";
-export { ClientHomePage, ClientProfilePage } from "./cliente";
+export { ClientHomePage, ClientProfilePage, ClientSearchPage } from "./cliente";
